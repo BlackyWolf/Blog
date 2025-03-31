438ba85ba766c424cffc88beff989272ee8904b5
Blacky Wolf <support@blackywolf.me>
2025-03-30 21:18:29 -0500

    Fixed column names for SQLite updatedAt column

--- code/src/lib/server/data/schema/pages.schema.ts
+++ code/src/lib/server/data/schema/pages.schema.ts
@@ -10,7 +10,7 @@ export const pages = sqliteTable("pages", {
     createdAt: integer("created_at", { mode: "timestamp" })
         .notNull()
         .default(sql`(CURRENT_TIMESTAMP)`),
-    updatedAt: integer("created_at", { mode: "timestamp" })
+    updatedAt: integer("updated_at", { mode: "timestamp" })
         .notNull()
         .default(sql`(CURRENT_TIMESTAMP)`)
         .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
--- code/src/lib/server/data/schema/posts.schema.ts
+++ code/src/lib/server/data/schema/posts.schema.ts
@@ -11,7 +11,7 @@ export const posts = sqliteTable("posts", {
     createdAt: integer("created_at", { mode: "timestamp" })
         .notNull()
         .default(sql`(CURRENT_TIMESTAMP)`),
-    updatedAt: integer("created_at", { mode: "timestamp" })
+    updatedAt: integer("updated_at", { mode: "timestamp" })
         .notNull()
         .default(sql`(CURRENT_TIMESTAMP)`)
         .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
