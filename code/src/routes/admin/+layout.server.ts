3c21177fd9e9fbb393f284fc248e4d635f22df72
Blacky Wolf <support@blackywolf.me>
2025-05-31 12:40:08 -0500

    Removed unneeded comments

--- code/src/routes/admin/+layout.server.ts
+++ code/src/routes/admin/+layout.server.ts
@@ -1,9 +1,5 @@
 import type { LayoutServerLoad } from "../$types";
 
-// export const prerender = false;
-// export const ssr = true;
-// export const csr = false;
-
 export const load: LayoutServerLoad = ({ url }) => {
     // This is to ensure the 'validateAdminUser' hook is run on every request
     // under the admin route.
